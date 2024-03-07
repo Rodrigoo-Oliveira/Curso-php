bd2af7ee64414a184bfa405c6714288fc2cca8e9
Rodrigoo-Oliveira <rodrigoodeoliveira.santos@gmail.com>
2024-03-07 16:05:06 -0300

    desafios

--- avancando/banco.php
+++ avancando/banco.php
@@ -5,7 +5,7 @@ $contasCorrentes = [
         'titular' => 'Maria',
         'saldo' => 10000
     ],
-    '123.456.789-11' => [
+    '123.456.689-11' => [
         'titular' => 'Alberto',
         'saldo' => 300
     ],
@@ -15,15 +15,49 @@ $contasCorrentes = [
     ]
 ];
 
+function exibeMensagem(string $mensagem)
+{
+    echo $mensagem . PHP_EOL;
+}
 
-$contasCorrentes['123.456.789-10']['saldo'] -= 500;
-if(500 > $contasCorrentes['123.456.789-11']['saldo']) {
-    echo "Você não pode sacar esse valor" . PHP_EOL;
-} else {
-    $contasCorrentes['123.456.789-11']['saldo'] -= 500;
-} 
+function depositar(array $conta, float $valorADepositar): array
+{
+    if ($valorADepositar > 0) {
+        $conta['saldo'] += $valorADepositar;
+    } else {
+        exibeMensagem("Depositos precisam ser positivos");
+    }
+    return $conta;
+}
 
+function sacar(array $conta, float $valorASacar): array
+{
+    if ($valorASacar > $conta['saldo']) {
+        exibeMensagem("Você não tem saldo suficiente");
+    } else {
+        $conta['saldo'] -= $valorASacar;
+    }
+
+    return $conta;
+}
+
+$contasCorrentes['123.456.789-10'] = sacar(
+    $contasCorrentes['123.456.789-10'],
+    500
+);
+
+$contasCorrentes['123.456.689-11'] = sacar(
+    $contasCorrentes['123.456.689-11'],
+    200
+);
+
+$contasCorrentes['123.256.789-12'] = depositar(
+    $contasCorrentes['123.256.789-12'],
+    900
+);
 
 foreach ($contasCorrentes as $cpf => $conta) {
-    echo $cpf . " " . $conta['titular'] . ' ' . $conta['saldo'] . PHP_EOL;
+    exibeMensagem(
+        "$cpf  {$conta['titular']} {$conta['saldo']}"
+    );
 }
