c8c568f78df649f0393477af4c37ade9109048e7
Rodrigoo-Oliveira <rodrigoodeoliveira.santos@gmail.com>
2024-03-10 22:21:00 -0300

    funções de ordenação

---
+++ avancando/notas2.php
@@ -0,0 +1,27 @@
+<?php
+
+$notas = [
+    'Ana' => 10,
+    'João' => 8,
+    'Maria' => 9,
+    'Roberto' => 7,
+    'Vinicius' => 6
+];
+
+/*
+sort($notas);  // Ordena no formato crescente
+var_dump($notas);
+*/
+
+/*
+arsort($notas);  // Ordena no formato decrescente
+var_dump($notas);
+*/
+
+/*
+ksort($notas);  // Ordena no formato crescente das chaves( No caso ordem alfabética)
+var_dump($notas);
+*/
+
+krsort($notas);  // Ordena no formato decrescente das chaves( No caso ordem alfabética)
+var_dump($notas);
