892efce3f491f0fac40b252d22c32ab857dbe269
Rodrigoo-Oliveira <rodrigoodeoliveira.santos@gmail.com>
2024-03-13 18:58:50 -0300

    Adicionar elemento

--- avancando/matriculas.php
+++ avancando/matriculas.php
@@ -17,5 +17,13 @@ $novosAlunos = [
 
 //$alunos2022 = array_merge($alunos2021, $novosAlunos); 
 $alunos2022 = [...$alunos2021, 'Rodrigo Oliveira', ...$novosAlunos];
+array_push($alunos2022, 'Alice', 'Bob', 'Charlie'); // Adicionando no final do array (adiiona vários elementos)
+$alunos2022[] = 'Luiz'; // Adicionando no final de uma outra forma (adiciona 1 elemento)
+
+array_unshift($alunos2022, 'Stephane', 'Rafaela'); // Adiciona no início do array
+
+echo array_shift($alunos2022) . PHP_EOL;
+
+echo array_pop($alunos2022) . PHP_EOL;
 
 var_dump($alunos2022);
