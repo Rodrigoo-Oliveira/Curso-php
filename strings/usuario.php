2c44e12c293ddaaf47d218f9418a08fe5169ab7b
Rodrigoo-Oliveira <rodrigoodeoliveira.santos@gmail.com>
2024-03-17 11:19:24 -0300

    Manipulando case

--- strings/usuario.php
+++ strings/usuario.php
@@ -1,9 +1,29 @@
 <?php
 
-$email = 'ana@alura.com.br';
+$email = 'vinícius@alura.com.br';
+$senha = 'áéíóú';
+
+/*
+Forma que não devemos fazer (modo de mostrar que string pode ser acessada como ARRAY)
+
+$cont = 0;
+for ($i = 0; $i < 3; $i++) {    
+    echo $senha[$i] . PHP_EOL; 
+    $cont++;
+}
+*/
+
+echo mb_strlen($senha) . PHP_EOL;
+
+if (strlen($senha) < 8) {    //strlen -> Retorna o tamanho da string em bytes e não em caracteres
+    echo 'Senha insegura' . PHP_EOL;
+}
+
 
 $posicao = strpos($email, '@');
 
-echo substr($email, 0, $posicao) . PHP_EOL; // substr -> Retorna parte de uma string 
+$usuario = substr($email, 0, $posicao); // substr -> Retorna parte de uma string 
+
+echo mb_strtoupper($usuario) . PHP_EOL;
 echo substr($email, $posicao + 1) . PHP_EOL;
 
