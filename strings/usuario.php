f09f8e4a83af69d0b5cac3e1e11bacfd4fd9865b
Rodrigoo-Oliveira <rodrigoodeoliveira.santos@gmail.com>
2024-03-17 23:30:46 -0300

    Separando string

--- strings/usuario.php
+++ strings/usuario.php
@@ -1,5 +1,6 @@
 <?php
 
+$nome = 'Vinícius Dias';
 $email = 'vinícius@alura.com.br';
 $senha = 'áéíóú';
 
@@ -26,4 +27,13 @@ $usuario = substr($email, 0, $posicao); // substr -> Retorna parte de uma string
 
 echo mb_strtoupper($usuario) . PHP_EOL;
 echo substr($email, $posicao + 1) . PHP_EOL;
+
+
+list($nome, $sobrenome) = explode(' ', $nome);
+
+echo 'Nome: ' . $nome . PHP_EOL;
+echo 'Sobrenome: ' . $sobrenome . PHP_EOL;
+
+$csv = 'Vinicius Dias,24,vinicius@alura.com.br';
+var_dump(explode(',',$csv));
 
