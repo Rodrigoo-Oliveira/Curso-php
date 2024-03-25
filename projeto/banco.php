f14957c035d17e2eb80a5c0440d45cf0405e8ace
Rodrigoo-Oliveira <rodrigoodeoliveira.santos@gmail.com>
2024-03-24 22:26:35 -0300

    Metodos privados

--- projeto/banco.php
+++ projeto/banco.php
@@ -3,11 +3,13 @@
 require_once 'src/Conta.php';
 
 $primeiraConta = new Conta(cpfTitular:'123.456.789-10', nomeTitular:'Vinicius Dias');
-var_dump($primeiraConta);
 $primeiraConta->deposita(500);
 $primeiraConta->saca(300);
 
 echo $primeiraConta->recuperaNomeTitular() . PHP_EOL;
 echo $primeiraConta->recuperaCPFTitular() . PHP_EOL;
 echo $primeiraConta->recuperaSaldo() . PHP_EOL;
+
+$segundaConta = new Conta('698.549.548-10', 'Ana');
+var_dump($segundaConta);
 
