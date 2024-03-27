9a1203876aa00a910b17a77048293d56fddb8810
Rodrigoo-Oliveira <rodrigoodeoliveira.santos@gmail.com>
2024-03-27 16:22:34 -0300

    Criando classe

--- projeto/banco.php
+++ projeto/banco.php
@@ -1,8 +1,10 @@
 <?php
 
 require_once 'src/Conta.php';
+require_once 'src/Titular.php';
 
-$primeiraConta = new Conta(cpfTitular:'123.456.789-10', nomeTitular:'Vinicius Dias');
+$vinicius = new Titular(cpf:'123.456.789-10', nome:'Vinicius Dias');
+$primeiraConta = new Conta($vinicius);
 $primeiraConta->deposita(500);
 $primeiraConta->saca(300);
 
@@ -10,9 +12,10 @@ echo $primeiraConta->recuperaNomeTitular() . PHP_EOL;
 echo $primeiraConta->recuperaCPFTitular() . PHP_EOL;
 echo $primeiraConta->recuperaSaldo() . PHP_EOL;
 
-$segundaConta = new Conta(cpfTitular:'698.549.548-10', nomeTitular:'Patricia');
+$patricia = new Titular(cpf:'698.549.548-10', nome:'Patricia');
+$segundaConta = new Conta($patricia);
 var_dump($segundaConta);
 
-$outra = new Conta('123', 'Abcdefg');
+$outra = new Conta(new Titular('123', 'Abcdefg'));
 unset($segundaConta);
 echo Conta::recuperaNumeroDeContas();
--- projeto/src/Conta.php
+++ projeto/src/Conta.php
@@ -3,16 +3,13 @@
 class Conta 
 {
 
-    private string $cpfTitular;        
-    private string $nomeTitular;
-    private float $saldo = 0;
-    private static $numeroDeContas = 0; // 
+    private $titular;        
+    private float $saldo;
+    private static $numeroDeContas = 0; 
 
-    public function __construct(string $cpfTitular, string $nomeTitular) 
+    public function __construct(Titular $titular) 
     {
-        $this->cpfTitular = $cpfTitular;
-        $this->validaNomeTitular($nomeTitular);
-        $this->nomeTitular = $nomeTitular;
+        $this->titular = $titular;
         $this->saldo = 0;
 
         self::$numeroDeContas++; // Para acessar atributos estáticos e possiveis metodos estáticos
@@ -59,22 +56,14 @@ class Conta
         return $this->saldo;
     }
 
-    public function recuperaCPFTitular():string
+    public function recuperaNomeTitular(): string
     {
-        return $this->cpfTitular;
+        return $this->titular->recuperaNome();
     }
 
-    public function recuperaNomeTitular():string
+    public function recuperaCpfTitular(): string
     {
-        return $this->nomeTitular;
-    }
-
-    private function validaNomeTitular(string $nomeTitular)
-    {
-        if (strlen($nomeTitular) < 5) {
-            echo "Nome precisa ter pelo menos 5 caracteres";
-            exit();
-        }
+        return $this->titular->recuperaCpf();
     }
 
     public static function recuperaNumeroDeContas(): int
