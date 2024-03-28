2f6c14922474742a2f32b5bc43d021d070881cca
Rodrigoo-Oliveira <rodrigoodeoliveira.santos@gmail.com>
2024-03-28 12:14:14 -0300

    Add classe endereço

--- projeto/banco.php
+++ projeto/banco.php
@@ -1,10 +1,12 @@
 <?php
 
 require_once 'src/Conta.php';
+require_once 'src/Endereco.php';
 require_once 'src/Titular.php';
 require_once 'src/CPF.php';
 
-$vinicius = new Titular(new CPF('123.456.789-10'), 'Vinicius Dias');
+$enderco = new Endereco(cidade:'Petrópolis', bairro:'um bairro', rua:'minha rua', numero:'71B');
+$vinicius = new Titular(new CPF('123.456.789-10'), 'Vinicius Dias', $enderco);
 $primeiraConta = new Conta($vinicius);
 $primeiraConta->deposita(500);
 $primeiraConta->saca(300); // isso é ok
@@ -13,10 +15,12 @@ echo $primeiraConta->recuperaNomeTitular() . PHP_EOL;
 echo $primeiraConta->recuperaCpfTitular() . PHP_EOL;
 echo $primeiraConta->recuperaSaldo() . PHP_EOL;
 
-$patricia = new Titular(new CPF('698.549.548-10'), 'Patricia');
+$patricia = new Titular(new CPF('698.549.548-10'), 'Patricia', $enderco);
 $segundaConta = new Conta($patricia);
 var_dump($segundaConta);
 
-$outra = new Conta(new Titular(new CPF('123.654.789-'), 'Abcdefg'));
+
+$outroEndereco = new Endereco('A', 'b', 'c', '1D');
+$outra = new Conta(new Titular(new CPF('123.654.789-01'), 'Abcdefg', $outroEndereco));
 unset($segundaConta);
 echo Conta::recuperaNumeroDeContas();
