2073d815e8897753b20a024caddab3bf1b30a22b
Rodrigoo-Oliveira <rodrigoodeoliveira.santos@gmail.com>
2024-04-01 15:50:40 -0300

    Interfaces

--- projeto/autenticacao.php
+++ projeto/autenticacao.php
@@ -2,15 +2,16 @@
 
 use Alura\Banco\Modelo\CPF;
 use Alura\Banco\Modelo\Funcionario\Diretor;
+use Alura\Banco\Modelo\Funcionario\Gerente;
 use Alura\Banco\Service\Autenticador;
 
 require_once 'autoload.php';
 
 $autenticador = new Autenticador();
-$umDiretor = new Diretor(
+$umDiretor = new Gerente(
     'João da Silva',
     new CPF('123.456.789-10'),
     salario:10000
 );
 
-$autenticador->tentaLogin($umDiretor, senha:'1234');
+$autenticador->tentaLogin($umDiretor, senha:'4321');
