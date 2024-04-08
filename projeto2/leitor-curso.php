d23ae15bc07fda9269f3ec15cc6a1273f9ff6b9c
Rodrigoo-Oliveira <rodrigoodeoliveira.santos@gmail.com>
2024-04-08 17:06:04 -0300

    Tamanho do arquivo

--- projeto2/leitor-curso.php
+++ projeto2/leitor-curso.php
@@ -1,5 +1,7 @@
 <?php
 
+/* solução para ler linha a linha
+
 $arquivo = fopen(filename:'curso.txt', mode:'r');
 
 while (!feof($arquivo)) {
@@ -7,5 +9,14 @@ while (!feof($arquivo)) {
 
     echo $curso;
 }
+
+*/
+
+$arquivo = fopen(filename:'curso.txt', mode:'r');
+
+$tamanhoArquivo = filesize('curso.txt'); // passa o nome do arquivo('curso.txt') e não o arquivo aberto ($arquivo = fopen(filename:'curso.txt', mode:'r');)
+$curso = fread($arquivo, $tamanhoArquivo);
+
+echo $curso;
 
 fclose($arquivo);
