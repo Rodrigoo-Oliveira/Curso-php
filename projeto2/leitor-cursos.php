61d932d44ba1fb122c75fc9cbc73e4df10993e6c
Rodrigoo-Oliveira <rodrigoodeoliveira.santos@gmail.com>
2024-04-08 20:36:48 -0300

    Lendo arquivos

--- projeto2/leitor-cursos.php
+++ projeto2/leitor-cursos.php
@@ -1,8 +1,11 @@
 <?php
 
-/* solução para ler linha a linha
+/*  
+Solução 1
 
-$arquivo = fopen(filename:'curso.txt', mode:'r');
+Para ler linha a linha e está abordagem serve para ler arquivos grandes
+
+$arquivo = fopen(filename:'lista-cursos.txt', mode:'r');
 
 while (!feof($arquivo)) {
     $curso = fgets($arquivo);
@@ -12,7 +15,10 @@ while (!feof($arquivo)) {
 
 */
 
-$arquivo = fopen(filename:'curso.txt', mode:'r');
+//Solução 2 
+//Para ler arquivos pequenos usar está abordagem e serve para arquivos menor de 128megas
+
+$arquivo = fopen(filename:'lista-cursos.txt', mode:'r');
 
 $tamanhoArquivo = filesize('curso.txt'); // passa o nome do arquivo('curso.txt') e não o arquivo aberto ($arquivo = fopen(filename:'curso.txt', mode:'r');)
 $curso = fread($arquivo, $tamanhoArquivo);
---
+++ projeto2/leitor-cursos2.php
@@ -0,0 +1,6 @@
+<?php
+
+
+$cursos = file_get_contents(filename:'lista-cursos.txt');
+
+echo $cursos;
