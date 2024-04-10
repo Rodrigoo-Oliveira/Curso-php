8e2b4ee7a9baeb3b1943da9c3926e7655603d5cd
Rodrigoo-Oliveira <rodrigoodeoliveira.santos@gmail.com>
2024-04-09 23:33:54 -0300

    Faciliatando a escrita

--- projeto2/escritor-cursos.php
+++ projeto2/escritor-cursos.php
@@ -1,9 +1,15 @@
 <?php
 
-$arquivo = fopen(filename:'cursos-php.txt', mode:'a'); // Modo de abertura 'w' sobrescreve o texto 
+/*
+$arquivo = fopen(filename:'cursos-php.txt', mode:'a+'); // Modo de abertura 'w' sobrescreve o texto 
                                                         // Modo de abertura 'a' coloca o cursos no final do arquivo
 $curso = "\nDesign Patterns PHP II: Boas práticas de progrmação";
 
 fwrite($arquivo, $curso);
 
 fclose($arquivo);
+*/
+
+$curso = "\nDesign Patterns PHP II: Boas práticas de progrmação";
+
+file_put_contents('cursos-php.txt', $curso, FILE_APPEND);
