d18153dc13c71bdf7f64cb0c22c2c5a0a5ce3649
Rodrigoo-Oliveira <rodrigoodeoliveira.santos@gmail.com>
2024-04-13 14:10:16 -0300

    SPL

---
+++ projeto2/spl.php
@@ -0,0 +1,24 @@
+<?php
+
+$arquivoCursos = new SplFileObject('cursos.csv');
+
+while (!$arquivoCursos->eof()) {
+    $linha = $arquivoCursos->fgetcsv(';');
+
+    echo $linha[0] . PHP_EOL;
+}
+
+/*
+
+$arquivo = new SplFileObject('cursos.csv');
+
+foreach ($arquivo as $linha) {
+    echo $linha;
+}
+
+*/
+
+$date = new DateTime();
+$date->setTimestamp($arquivoCursos->getATime());
+
+echo $date->format('d/m/Y');
