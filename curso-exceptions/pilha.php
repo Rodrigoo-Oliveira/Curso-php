c3bc338170066da67b8f778cb624d3042d29c110
Rodrigoo-Oliveira <rodrigoodeoliveira.santos@gmail.com>
2024-04-14 22:16:42 -0300

    Pegando múltiplas exceções

--- curso-exceptions/pilha.php
+++ curso-exceptions/pilha.php
@@ -3,19 +3,26 @@
 function funcao1()
 {
     echo 'Entrei na função 1' . PHP_EOL;
-    try{
-        $arrayFixo = new SplFixedArray(2);
-        $arrayFixo[3] = 'Valor';
-    } catch (RuntimeException $problema) {
-        echo "Aconteceu um erro na função 1" . PHP_EOL;
-    }
+    try {
     funcao2();
+    } catch (RuntimeException | DivisionByZeroError $erroOuExceção) {
+        echo $erroOuExceção->getMessage() . PHP_EOL;
+        echo $erroOuExceção->getLine() . PHP_EOL;
+        echo $erroOuExceção->getTraceAsString() . PHP_EOL;
+    }
+
     echo 'Saindo da função 1' . PHP_EOL;
+
 }
 
 function funcao2()
 {
     echo 'Entrei na função 2' . PHP_EOL;
+
+    $divisao = intdiv(5,0);
+    $arrayFixo = new SplFixedArray(2);
+    $arrayFixo[3] = 'Valor';
+
     for ($i = 1; $i <= 5; $i++) {
         echo $i . PHP_EOL;
     }
