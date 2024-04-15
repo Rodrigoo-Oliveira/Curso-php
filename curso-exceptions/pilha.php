4d2e26eb8b5d5d370cc527a4b0b848bf517bcc8c
Rodrigoo-Oliveira <rodrigoodeoliveira.santos@gmail.com>
2024-04-14 23:22:02 -0300

    Lançando uma exceção

--- curso-exceptions/pilha.php
+++ curso-exceptions/pilha.php
@@ -5,10 +5,15 @@ function funcao1()
     echo 'Entrei na função 1' . PHP_EOL;
     try {
     funcao2();
-    } catch (RuntimeException | DivisionByZeroError $erroOuExceção) {
+    } catch (RuntimeException | DivisionByZeroError $erroOuExceção) { //Através de um | (pipe) na cláusula catch, podemos capturar mais de uma exceção. Com isso, podemos ter um único bloco para tratar uma ou mais exceções.
         echo $erroOuExceção->getMessage() . PHP_EOL;
         echo $erroOuExceção->getLine() . PHP_EOL;
         echo $erroOuExceção->getTraceAsString() . PHP_EOL;
+        throw new RuntimeException(
+            'Exceção foi tratada, mas, pega aí',
+            1,
+            $erroOuExceção
+        );
     }
 
     echo 'Saindo da função 1' . PHP_EOL;
@@ -19,13 +24,8 @@ function funcao2()
 {
     echo 'Entrei na função 2' . PHP_EOL;
 
-    $divisao = intdiv(5,0);
-    $arrayFixo = new SplFixedArray(2);
-    $arrayFixo[3] = 'Valor';
-
-    for ($i = 1; $i <= 5; $i++) {
-        echo $i . PHP_EOL;
-    }
+    throw new RuntimeException();
+    
     echo 'Saindo da função 2' . PHP_EOL;
 }
 
