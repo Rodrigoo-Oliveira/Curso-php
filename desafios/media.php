75052a88d0240dbb8ee6dbb89affdd0a8a6bcd22
Rodrigoo-Oliveira <rodrigoodeoliveira.santos@gmail.com>
2024-04-22 16:34:04 -0300

    Praticando

---
+++ desafios/media.php
@@ -0,0 +1,22 @@
+<?php
+/*
+$nota1 = 10;
+$nota2 = 7.3;
+$nota3 = 5;
+
+$somaNotas = ($nota1 + $nota2 + $nota3) / 3;
+
+echo $somaNotas;
+*/
+
+$notas = array(10, 7.3, 5);
+$cont = 0;
+$quantNotas = count($notas);
+
+for ($i=0; $i < $quantNotas; $i++) {
+    $cont += $notas[$i]; 
+}
+
+$media = $cont / $quantNotas;
+
+echo "Média das notas: " . $media;
---
+++ screen-match/screen-match.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "Bem-vindo(a) ao screen match" . PHP_EOL;
+
+$nomeFilme = "Top Gun - Maverick";
+$anoLancamento = $argv[1] ?? 2022;
+$notaFilme = 8.8;
+$incluindoNoPlano = true;
